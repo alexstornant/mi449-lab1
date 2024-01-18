8d951a0f383da5b61fcfa06df7210e946f140fc2
alexstornant <stornant.alex@gmail.com>
2024-01-18 15:13:33 -0500

    reconfigured file structure

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,3 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy('screen.css')
-    return {
-      passthroughFileCopy: true
-    }
-  }
+    eleventyConfig.addPassthroughCopy("bundle");
+  };
